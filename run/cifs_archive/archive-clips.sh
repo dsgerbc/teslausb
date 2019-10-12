994c18e60adc6ce0330ad292550fff2268fb46bd
dsgerbc <49077461+dsgerbc@users.noreply.github.com>
2019-10-11 22:48:52 -0400

    Update archive-clips.sh

--- run/cifs_archive/archive-clips.sh
+++ run/cifs_archive/archive-clips.sh
@@ -6,36 +6,30 @@ NUM_FILES_MOVED=0
 NUM_FILES_FAILED=0
 NUM_FILES_DELETED=0
 
-function keep_car_awake() {
-  # If the tesla_api.py script is installed, send the car a wake_up command.
-  if [ -f /root/bin/tesla_api.py ]
-  then
-    /root/bin/tesla_api.py wake_up_vehicle >> "$LOG_FILE"
-  fi
-}
-
 function connectionmonitor {
   while true
   do
-    if timeout 5 /root/bin/archive-is-reachable.sh $ARCHIVE_HOST_NAME
-    then
-      sleep 2
-    elif timeout 5 /root/bin/archive-is-reachable.sh $ARCHIVE_HOST_NAME # try one more time
-    then
-      sleep 2
-    else
-      log "connection dead, killing archive-clips"
-      # The archive loop might be stuck on an unresponsive server, so kill it hard.
-      # (should be no worse than losing power in the middle of an operation)
-      kill -9 $1
-      return
-    fi
+    for i in $(seq 1 10)
+    do
+      if timeout 3 /root/bin/archive-is-reachable.sh $ARCHIVE_HOST_NAME
+      then
+        # sleep and then continue outer loop
+        sleep 5
+        continue 2
+      fi
+    done
+    log "connection dead, killing archive-clips"
+    # The archive loop might be stuck on an unresponsive server, so kill it hard.
+    # (should be no worse than losing power in the middle of an operation)
+    kill -9 $1
+    return
   done
 }
 
 function moveclips() {
   ROOT="$1"
   PATTERN="$2"
+  SUB=$(basename $ROOT)
 
   if [ ! -d "$ROOT" ]
   then
@@ -47,10 +41,10 @@ function moveclips() {
   do
     if [ -d "$ROOT/$file_name" ]
     then
-      log "Creating output directory '$file_name'"
-      if ! mkdir -p "$ARCHIVE_MOUNT/$file_name"
+      log "Creating output directory '$SUB/$file_name'"
+      if ! mkdir -p "$ARCHIVE_MOUNT/$SUB/$file_name"
       then
-        log "Failed to create '$file_name', check that archive server is writable and has free space"
+        log "Failed to create '$SUB/$file_name', check that archive server is writable and has free space"
         return
       fi
     elif [ -f "$ROOT/$file_name" ]
@@ -58,23 +52,23 @@ function moveclips() {
       size=$(stat -c%s "$ROOT/$file_name")
       if [ $size -lt 100000 ]
       then
-        log "'$file_name' is only $size bytes"
+        log "'$SUB/$file_name' is only $size bytes"
         rm "$ROOT/$file_name"
         NUM_FILES_DELETED=$((NUM_FILES_DELETED + 1))
       else
-        log "Moving '$file_name'"
+        log "Moving '$SUB/$file_name'"
         outdir=$(dirname "$file_name")
-        if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$outdir"
+        if mv -f "$ROOT/$file_name" "$ARCHIVE_MOUNT/$SUB/$outdir"
         then
-          log "Moved '$file_name'"
+          log "Moved '$SUB/$file_name'"
           NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
         else
-          log "Failed to move '$file_name'"
+          log "Failed to move '$SUB/$file_name'"
           NUM_FILES_FAILED=$((NUM_FILES_FAILED + 1))
         fi
       fi
     else
-      log "$file_name not found"
+      log "$SUB/$file_name not found"
     fi
   done <<< $(cd "$ROOT"; find $PATTERN)
 }
